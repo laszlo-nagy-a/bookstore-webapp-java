3417e3f9943f735b4fd89598633e4e4770ebe33d
Laszlo Nagy <laszlo.n620@gmail.com>
2024-10-15 19:08:33 +0200

    URL útvonal elkészítése XML segítségével

--- bookstore/src/main/java/com/pluralsight/ControllerServlet.java
+++ bookstore/src/main/java/com/pluralsight/ControllerServlet.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
+//import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ControllerServlet
  */
-@WebServlet("/ControllerServlet")
+//@WebServlet("/ControllerServlet")
 public class ControllerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
