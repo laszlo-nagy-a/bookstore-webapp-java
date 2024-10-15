de71483e59101ba5c2f6aea3c319a7878b52e736
Laszlo Nagy <laszlo.n620@gmail.com>
2024-10-15 21:28:41 +0200

    JSP használata és adatátadás

--- bookstore/src/main/java/com/pluralsight/ControllerServlet.java
+++ bookstore/src/main/java/com/pluralsight/ControllerServlet.java
@@ -2,9 +2,11 @@ package com.pluralsight;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-//import javax.servlet.annotation.WebServlet;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,15 +14,20 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ControllerServlet
  */
-//@WebServlet("/ControllerServlet")
+@WebServlet("/books/*")
 public class ControllerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private ArrayList<String> bookTitles = new ArrayList<String>();
        
     /**
      * @see HttpServlet#HttpServlet()
      */
     public ControllerServlet() {
         super();
+        bookTitles.add("Lord of the Files");
+        bookTitles.add("A tale of two servers");
+        bookTitles.add("To catch a throw");
+        
         // TODO Auto-generated constructor stub
     }
 
@@ -28,17 +35,24 @@ public class ControllerServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		PrintWriter output = response.getWriter();
-		output.println("Hello ControllerServlet");
+		request.setAttribute("book_titles", bookTitles);
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/BooksList.jsp");
+		dispatcher.forward(request, response);	
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		PrintWriter output = response.getWriter();
+		
+		String title = request.getParameter("title");
+		String author = request.getParameter("author");
+		
+		output.println("The book title is: " + title);
+		output.println("The book author is: " + author);
 	}
 
 }
